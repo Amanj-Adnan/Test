1b927d59ed5523467f02f113bda173f39d7cab8f
Amanj Adnan <52677775+Amanj-Adnan@users.noreply.github.com>
2022-08-13 18:41:18 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,1 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+
