309b9d5da9b42c7579ad6f72e7e9ffad20b42ccb
Akkadian <amanjadnan7@gmail.com>
2022-08-14 10:05:44 +0300

    commit

--- db/migrate/20220810131211_drop_wrok_in_and_fix_workflow.rb
+++ db/migrate/20220810131211_drop_wrok_in_and_fix_workflow.rb
@@ -1,6 +1,5 @@
 class DropWrokInAndFixWorkflow < ActiveRecord::Migration[6.1]
   def change
-    drop_table :work_ins
     add_reference :users , :user_workflow , index: true,:default => 0, :null => false
   end
 end
